0fa9327651de2e3a2e61e862fdf52534d91a23e0
jonas8217 <Jonas82173000@gmail.com>
2021-12-12 13:53:25 +0100

    finished day 11

--- day11.py
+++ day11.py
@@ -4,11 +4,6 @@ inp = getday(11)
 
 octogrid = [[[int(octo),0] for octo in row] for row in inp]
 
-octogrid = [
-    [[2,0],[8,0],[7,0]],
-    [[6,0],[1,0],[7,0]],
-    [[5,0],[1,0],[2,0]]
-]
 
 print('\n'.join(map(str,octogrid)))
 
@@ -25,30 +20,59 @@ def flash(grid,x,y,currentflashes):
     for i in range(fx,tx+1):
         for j in range(fy,ty+1):
             grid[j][i][0] += 1
-            if grid[j][i][0] >= 9 and grid[j][i][1] != 1:
+            if grid[j][i][0] > 9 and grid[j][i][1] != 1:
                 grid,currentflashes = flash(grid,i,j,currentflashes)
     return grid,currentflashes
 
 
-#g,f = flash(octogrid,1,1,0)
-#rint('\n'.join(map(str,g)))
+def allFlash(grid):
+    allflash = True
+    for row in grid:
+        for octo in row:
+            if octo[1] != 1:
+                allflash = False
+    return allflash
 
-for i in range(5):
+
+# part 1
+for i in range(100):
     for y in range(ymax+1):
         for x in range(xmax+1):
             octogrid[y][x][0] += 1
-            if octogrid[y][x][0] >= 9 and octogrid[y][x][1] != 1:
+            if octogrid[y][x][0] > 9 and octogrid[y][x][1] != 1:
                 octogrid,flashes = flash(octogrid,x,y,0)
                 totalflashes += flashes
-
-    print('\n'.join(map(str,octogrid)))
 
     # reset flash
     for y in range(ymax+1):
         for x in range(xmax+1):
             if octogrid[y][x][1] == 1:
                 octogrid[y][x] = [0,0]
-    print()
-    print('\n'.join(map(str,octogrid)))
 
 print(totalflashes)
+
+
+# part 2
+octogrid = [[[int(octo),0] for octo in row] for row in inp]
+
+
+steps = 0
+while True:
+    steps += 1
+    for y in range(ymax+1):
+        for x in range(xmax+1):
+            octogrid[y][x][0] += 1
+            if octogrid[y][x][0] > 9 and octogrid[y][x][1] != 1:
+                octogrid,flashes = flash(octogrid,x,y,0)
+                totalflashes += flashes
+
+    if allFlash(octogrid):
+        break
+
+    # reset flash
+    for y in range(ymax+1):
+        for x in range(xmax+1):
+            if octogrid[y][x][1] == 1:
+                octogrid[y][x] = [0,0]
+
+print(steps)
